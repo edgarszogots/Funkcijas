ddb9b905234635d7f823764e06b0303fbf900b9f
augs31 <100840904+augs31@users.noreply.github.com>
2022-03-03 13:08:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
 
+
 using namespace std;
-int process (int x, int y, int & max, double & avg) 
-{
-//Iztrūkst kods!!!!!!!!!!!!!!!
+int process (int x, int y, int & max, double & avg) {
+ avg =(x+y)/2;
+if (x>y) { 
+  max = x;
+  starp = x-y;
 }
+else {
+  max = y;
+  starp = y;
+  return starp
+    }
+
 int main() {
 	int a,b,liel;
 	double vid;
