4bf8e1cc634cbd62d48846169aeef60b6cc42d8a
dpushnakova <100840861+dpushnakova@users.noreply.github.com>
2022-03-03 13:08:50 +0200

    Update main.cpp
    b

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 
-
 using namespace std;
-int process (int x, int y, int & max, double & avg) 
-{
-//Iztrūkst kods!!!!!!!!!!!!!!!
+int process (int x, int y, int & max, double & avg) {
+  int starpiba;
+  avg = (x+y)/2;
+  if(x > y){
+    max = x;
+    starpiba = x-y; 
+  }
+  else{
+    max = y;
+    starpiba = y;
+  }
+  return starpiba;  
 }
+
 int main() {
 	int a,b,liel;
 	double vid;
